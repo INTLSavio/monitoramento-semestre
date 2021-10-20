ecac69b5199de6d09bf3e401054090776c9c07f4
INTLSavio <savio.leite@gec.inatel.br>
2021-10-20 11:37:37 -0300

    Adicionado Sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import Sidebar from './components/Sidebar';
 
 function App() {
+  const [showSidebar, setShowSidebar] = useState(true)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Sidebar showSidebar={showSidebar} />
+      <section className="home-section">
+        <div className="home-content">
+          <i className='bx bx-menu' onClick={() => setShowSidebar(!showSidebar)} ></i>
+          <span className="text">Página Inicial</span>
+        </div>
+        </section>
+    </>
   );
 }
 
---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import './styles.css'
+
+interface SidebarProps {
+    showSidebar: boolean;
+}
+
+function Sidebar({ showSidebar }: SidebarProps) {
+
+    return(
+        <div className={showSidebar ? "sidebar close" : "sidebar"}>
+        <div className="logo-details">
+          <i className='bx bxl-c-plus-plus'></i>
+          <span className="logo_name">CodingLab</span>
+        </div>
+        <ul className="nav-links">
+          <li>
+            <a href="/">
+            <i className='bx bxs-home'></i>
+              <span className="link_name">Página Inicial</span>
+            </a>
+          </li>
+          <li>
+            <a href="/">
+            <i className='bx bxs-calculator' ></i>
+              <span className="link_name">Calculadora</span>
+            </a>
+          </li>
+          <li>
+            <a href="/">
+            <i className='bx bxs-book-alt' ></i>
+              <span className="link_name">Estudos</span>
+            </a>
+          </li>
+          <li>
+            <a href="/">
+            <i className='bx bxs-calendar' ></i>
+              <span className="link_name">Provas</span>
+            </a>
+          </li>
+          
+        </ul>
+      </div>
+    );
+}
+
+export default Sidebar;
